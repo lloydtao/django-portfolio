9eb418fa5d93d5c4f0305923684134bdc16ba0f3
LloydTao <LL23@live.co.uk>
2020-05-24 14:52:19 +0100

    Remove url field from Skill model

--- src/portfolio/models.py
+++ src/portfolio/models.py
@@ -16,7 +16,6 @@ class Project(models.Model):
 class Skill(models.Model):
     title   = models.CharField(max_length=64)
     date    = models.date = models.DateTimeField(default=timezone.now)
-    url     = models.CharField(max_length=64)
     content = models.CharField(max_length=128)
 
 class Education(models.Model):
