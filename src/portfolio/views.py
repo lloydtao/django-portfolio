e71bd14ca77354002bafcbbc07c212797e83432a
LloydTao <LL23@live.co.uk>
2020-05-24 22:46:02 +0100

    Return dictionary of paradigms as Skills queryset

--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -25,4 +25,21 @@ class EducationFeedView(ListView):
 class SkillsFeedView(ListView):
     model = Skill
     template_name = 'portfolio/skills.html'
-    context_object_name = 'skills'
+    context_object_name = 'paradigms'
+
+    def get_queryset(self):
+        queryset = {
+            'Software Engineering': Skill.objects.filter(paradigm='SE'),
+            'Web Development':      Skill.objects.filter(paradigm='WD'),
+            'Graphics and Media':   Skill.objects.filter(paradigm='GM'),
+            'Frameworks':           Skill.objects.filter(paradigm='FW'),
+        }
+        # Paradigms are unlikely to scale, but here's a scalable version.
+        """
+        queryset = {}
+        for paradigm in Skill.PARADIGM_CHOICES:
+            key = paradigm[0]
+            display = paradigm[1]
+            queryset[display] = Skill.objects.filter(paradigm=key)
+        """
+        return queryset
